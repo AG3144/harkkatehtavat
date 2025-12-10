2a2dea4ac0a0285c6abcc829e9ef6d03704bfd0f
Teemu Sairi <ag3144@student.jamk.fi>
2025-12-10 14:26:12 +0200

    tehtävät 1.1-1.3 tehty

--- testi/app.test.js
+++ testi/app.test.js
@@ -1,16 +1,20 @@
-/* Importeilla tuodaan käyttöön testausfunktiot vitest-kirjastosta, ja tuodaan mukaan itse testattava koodi eli laskin.js -tiedosto. */
-import { describe, test, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import ravintola from '../ravintola/ravintola.js';
 
-describe('Ravintola- sovelluksen testaus'),
-  function () {
-    it('should return correct sum from laskeLasku when customer picks starter and main course and dessert and no drink', function () {
-      expect(ravintola.laskeLasku(true, true, true)).toBe(17);
-    });
+describe('Ravintola- sovelluksen testaus', function () {
+  it('should return correct sum from laskeLasku when customer picks starter and main course and dessert and no drink', function () {
+    expect(ravintola.laskeLasku(true, true, true)).toBe(17);
+  });
 
-    it('should return a value from one of the arrays in Ravintola(alkuruoat, paaruoat, jalkiruoat, juomat))', () => {
-      const testiArvotaulokosta = ravintola.palautaTaulukonSatunnainenArvo(
-        ravintola.juomat
-      );
-    });
-  };
+  it('should return a value from one of the arrays in Ravintola (e.g. juomat)', () => {
+    const kohdeTaulukko = ravintola.paaruoat;
+    const palautettuArvo =
+      ravintola.palautaTaulukonSatunnainenArvo(kohdeTaulukko);
+    expect(kohdeTaulukko).toContain(palautettuArvo);
+  });
+  it('should return an Array of orders', () => {
+    const tilaukset = ravintola.syoRavintolassa(3);
+    expect(tilaukset).toBeInstanceOf(Array);
+  });
+  it;
+});
